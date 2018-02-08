39475c575f1c65032e237333cab216c66ffbf95f
yothinix <m@yothinix.com>
2018-02-08 19:32:24 +0700

    Using slice to get new list instead of copy and remove :rocket:

--- main.py
+++ main.py
@@ -11,9 +11,8 @@ class Banana(object):
 
 
 def get_in_class(obj, keys = []):
-    new_key = keys[:]
     if hasattr(obj, keys[0]):
-        new_key.remove(keys[0])
+        new_key = keys[1:]
         new_obj = getattr(obj, keys[0])
         if new_key:
             return get_in_class(new_obj, new_key)
