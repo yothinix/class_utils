6e620f241c2dca54c639d982cd399f2f61291a12
yothinix <m@yothinix.com>
2018-03-27 08:49:19 +0700

    compared two version :rocket:

--- main.py
+++ main.py
@@ -10,15 +10,28 @@ class Banana(object):
     apple = Apple()
 
 
-def get_in_class(obj, keys = []):
-    if hasattr(obj, keys[0]):
-        new_key = keys[1:]
-        new_obj = getattr(obj, keys[0])
-        if new_key:
-            return get_in_class(new_obj, new_key)
-        return new_obj
+def get_in_class(obj, keys=[], default='return-default'):
+    if not hasattr(obj, keys[0]):
+        return default
+    new_obj = getattr(obj, keys[0])
+    if keys[1:]:
+        return get_in_class(new_obj, keys[1:])
+    return new_obj
+
+
+def get_in_class_recursion(obj, keys, default='return-default'):
+    if keys == []:
+        return obj
+    name = keys[0]
+    if hasattr(obj, name):
+        return get_in_class_recursion(getattr(obj, name), keys[1:])
+    else:
+        return default
 
 
 b = Banana()
 
 print(get_in_class(b, ['apple', 'orange', 'mango']))
+print(get_in_class(b, ['apple', 'orange', 'banana']))
+print(get_in_class_recursion(b, ['apple', 'orange', 'mango']))
+print(get_in_class_recursion(b, ['apple', 'orange', 'banana']))
